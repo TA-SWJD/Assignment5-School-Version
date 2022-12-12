1a86ff6d911e54e8938989fc893b3e3624c85b6d
jimhuang <jim.hoooo2@gmail.com>
2022-12-12 13:51:02 -0500

    Login-page

--- vite-project/src/views/homeview.vue
+++ vite-project/src/views/homeview.vue
@@ -8,7 +8,7 @@ const username = ref("");
 const password = ref("");
 
 const login = () => {
-    if (username.value === "Tymon" && password.value === "Assignment5") {
+    if (username.value === "Sunnyeatshit" && password.value === "hornyass") {
         router.push("./login");
     } else {
         error.value = true;
@@ -22,16 +22,16 @@ const login = () => {
         <button type="button">Movie</button>
     </div> -->
     <div class="box"></div>
-    <div class=" homepage">
+    <div class="homepage">
         <img src="https://cdn-images-1.medium.com/max/1200/1*ty4NvNrGg4ReETxqU2N3Og.png" width="140" height="100">
         <p>Welcome to Netflix <br> Unlimited movies, TV shows, and more.</p>
         <form @submit.prevent="login()">
             <input type="text" placeholder="Username" v-model="username" />
             <input type="password" placeholder="Password" v-model="password" />
-            <input type="submit" value="Login" />
+            <input type="submit" value="Login" id="button"/>
         </form>
-        <div v-if="error">
-            <p>Incorrect Username/Password!</p>
+        <div class="error" v-if="error">
+            Invalid Username or Password
         </div>
     </div>
 </template>
@@ -73,6 +73,35 @@ const login = () => {
 
 .homepage > p {
     text-align: center;
+}
+.homepage > .error{
+    border-radius: 20px;
+    color:red;
+    font-size: 20px;
+    margin-top: 20px;
+    font-family: monospace;
+    background-color: black;
+}
+
+.homepage > form > input {
+    padding: 5px;
+    border-radius: 20px;
+    width: 200px;
+    height: 25px;
+}
+
+#button {
+    width: 215px;
+    height: 30px;
+    margin-top: 10px;
+    font-family: Georgia, 'Times New Roman', Times, serif;
+    background-color: white;
+    color: black;
+}
+
+#button:hover {
+    background-color: black;
+    color: white;
 }
 
 form {
